a2132bab3d6427077a76c91f6478f872e01695d4
Don Durrance <dev@asvattha.com>
2012-05-18 02:28:29 -0700

    removed obsoleted code from users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,6 @@
 module UsersHelper
-  
+
   def current_user
-    if session.nil? then 
-      #session = false 
-    end
     if session[:user].present? then User.find(session[:user][:id]) end
   end
 
